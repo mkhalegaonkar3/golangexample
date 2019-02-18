49cc1f1725191a8aad4aff7af1f1875efb14f8c7
mkhalegaonkar3 <mkhalegaonkar3@gmail.com>
2019-02-19 00:42:49 +0530

    ninja exercise 4

---
+++ ninja_exercise_4/multidiamensionalSlice.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	m1:=[]string{"James", "Bond", "Shaken, not stirred"}
+	m2:=[]string{"Miss", "Moneypenny", "Helloooooo, James."}
+	multiSlice:=[][]string{m1,m2}
+	fmt.Println(multiSlice)
+
+	for i,slice :=range multiSlice{
+		fmt.Println("Record",i)
+		for j,v:= range slice{
+			fmt.Println("\t",j,v)
+		}
+
+	}
+}
